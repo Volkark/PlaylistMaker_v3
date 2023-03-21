33434dcb209416c82fff54f051d765c3204ff251
VolKark <vvkarkov@yandex.ru>
2023-03-22 01:52:34 +0500

    PlaylistMaker_v3 Commit 08 / Sprint 13

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -5,9 +5,9 @@ data class Track (
     val trackName: String,          // Название композиции
     val artistName: String,         // Имя исполнителя
     val collectionName: String,     // Название альбома
-    val releaseDate: String,          // Год релиза трека
+    val releaseDate: String,        // Год релиза трека
     val primaryGenreName: String,   // Жанр трека
-    val country: String,            // Страна исполненителя
+    val country: String,            // Страна исполнителя
     val trackTimeMillis: Long,      // Продолжительность трека в миллисекндах
     val artworkUrl100: String,      // Ссылка на изображение обложки
 ) {
